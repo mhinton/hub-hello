3a19edb16dabd67a9b9bac419feab65734cd47d3
Matthew Hinton <mhinton@gmail.com>
2024-07-16 13:26:37 -0500

    Add some content to test with

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,19 @@
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">
+          home
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/demo">
+          demo
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
   <NuxtPage />
 </template>
---
+++ app/pages/demo.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Demo</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
